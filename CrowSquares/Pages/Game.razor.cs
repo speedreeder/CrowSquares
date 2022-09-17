ff7b7519ee7bbb7b3eb2104c4d5013b16f0e265e
Alex Reeder <alexgreeder@outlook.com>
2022-09-17 16:28:49 -0400

    Add project files.

---
+++ CrowSquares/Pages/Game.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace CrowSquares.Pages
+{
+    public class GameBase :  ComponentBase
+    {
+        public string Color = "blue";
+        public string RowStyle = "";
+
+        public void ChangeColor()
+        {
+            if(Color == "blue")
+            {
+                Color = "red";
+                return;
+            }
+
+            Color = "blue";
+            return;
+        }
+    }
+}
